b6982612613d59d4cba18063ea82dd752d7350f3
Brian Hilnbrand <brian.hilnbrand@cora.aero>
2019-07-30 12:59:15 -0700

    Spline, not quite working

--- src/main.cpp
+++ src/main.cpp
@@ -7,6 +7,7 @@
 #include "Eigen-3.3/Eigen/QR"
 #include "helpers.h"
 #include "json.hpp"
+#include "spline.h"
 
 // for convenience
 using nlohmann::json;
@@ -49,9 +50,6 @@ int main() {
     map_waypoints_dx.push_back(d_x);
     map_waypoints_dy.push_back(d_y);
   }
-
-  int lane = 1;
-  double ref_vel = 49.5;
 
   h.onMessage([&map_waypoints_x,&map_waypoints_y,&map_waypoints_s,
                &map_waypoints_dx,&map_waypoints_dy]
@@ -100,16 +98,89 @@ int main() {
            *   sequentially every .02 seconds
            */
 
+          static int lane = 1;
+          static double ref_vel = 49.5;
+
+          vector<double> ptsx;
+          vector<double> ptsy;
+
+          double ref_x = car_x;
+          double ref_y = car_y;
+          double ref_yaw = deg2rad(car_yaw);
+
+          if (prev_size < 2){
+            double prev_car_x = car_x - cos(car_yaw);
+            double prev_car_y = car_y - sin(car_yaw);
+
+            ptsx.push_back(prev_car_x);
+            ptsx.push_back(car_x);
+
+            ptsy.push_back(prev_car_y);
+            ptsy.push_back(car_y);
+          }
+          else{
+            ref_x = previous_path_x[prev_size-1];
+            ref_y = previous_path_y[prev_size-1];
+
+            double ref_x_prev = previous_path_x[prev_size-2];
+            double ref_y_prev = previous_path_y[prev_size-2];
+            ref_yaw = atan2(ref_y - ref_y_prev, ref_x - ref_x_prev);
+
+            ptsx.push_back(ref_x_prev);
+            ptsx.push_back(car_x);
+
+            ptsy.push_back(ref_y_prev);
+            ptsy.push_back(car_y);
+          }
+
+          for (size_t d = 1; d < 3; ++d){
+            vector<double> next_wp = getXY(car_s+(d*30), 2+4*lane, map_waypoints_s, map_waypoints_x, map_waypoints_y);
+            ptsx.push_back(next_wp[0]);
+            ptsy.push_back(next_wp[1]);
+          }
+
+          for (size_t i = 0; i < ptsx.size(); ++i){
+            double shift_x = ptsx[i]-ref_x;
+            double shift_y = ptsy[i]-ref_y;
+
+            ptsx[i] = (shift_x*cos(0-ref_yaw)-shift_y*sin(0-ref_yaw));
+            ptsy[i] = (shift_x*sin(0-ref_yaw)+shift_y*cos(0-ref_yaw));
+          }
+
+          tk::spline s;
+          s.set_points(ptsx, ptsy);
+
           vector<double> next_x_vals, next_y_vals;
 
-          double dist_inc = 0.3;
-          for (int i = 0; i < 50; ++i) {
-            double next_s = car_s+(i+1)*dist_inc;
-            double next_d = 6;
-            vector<double> xy = getXY(next_s, next_d, map_waypoints_s, map_waypoints_x, map_waypoints_y);
+          for (size_t i = 0; i < previous_path_x.size(); ++i){
+            next_x_vals.push_back(previous_path_x[i]);
+            next_y_vals.push_back(previous_path_y[i]);
+          }
 
-            next_x_vals.push_back(xy[0]);
-            next_y_vals.push_back(xy[1]);
+          double target_x = 30.0;
+          double target_y = s(target_x);
+          double target_dist = sqrt(target_x*target_x+target_y*target_y);
+
+          double x_add_on = 0;
+
+          for (size_t i = 1; i <= 50-previous_path_x.size(); ++i){
+            double N = (target_dist/(0.02*ref_vel/2.24/*mph->meters/sec*/));
+            double x_point = x_add_on+target_x/N;
+            double y_point = s(x_point);
+
+            x_add_on = x_point;
+
+            double x_ref = x_point;
+            double y_ref = y_point;
+
+            x_point = (x_ref*cos(ref_yaw)-y_ref*sin(ref_yaw));
+            y_point = (x_ref*sin(ref_yaw)+y_ref*cos(ref_yaw));
+
+            x_point += ref_x;
+            y_point += ref_y;
+
+            next_x_vals.push_back(x_point);
+            next_y_vals.push_back(y_point);
           }
 
           msgJson["next_x"] = next_x_vals;
